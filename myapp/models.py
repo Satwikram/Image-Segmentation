574b027e8b4a944d53d30d19289a7f89551d79ae
Satwik Ram <satwikram29@gmail.com>
2021-05-02 17:00:38 +0530

    Models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+def directory_path(instance, filename):
+    return 'uploads/images/{0}-{1}'.format(filename, instance.pk)
+
+def prediction_path(instance, filename):
+    return 'uploads/predictions/{0}-{1}'.format(filename, instance.pk)
+
+
+class Segmentation(models.Model):
+
+    image = models.ImageField(upload_to = directory_path)
+    prediction = models.ImageField(upload_to = prediction_path)
