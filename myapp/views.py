b49e9d6c30e74d5ccbed5f8cf67501d021464955
Satwik Ram <satwikram29@gmail.com>
2021-05-02 21:32:59 +0530

    Update

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from .models import *
+from .serializers import *
+import requests
+from io import StringIO
+import urllib.request
+import tensorflow as tf
+import numpy as np
 
 class ImageSegmentationAPIView(APIView):
 
-    def post(self, request):
-        print(request.data)
+    def get(self, request):
 
-        return Response({"Status": "Sucess"})
+        data = Segmentation.objects.all()
+
+        serializer = SegmentationSerializer(data, many = True)
+
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def post(self, request):
+
+        image = request.data['image']
+
+        print(image.name)
+
+        model = tf.keras.models.load_model('segment.hdf5')
+
+        return Response({"Status": "Sucess!"})
