cac55b510e52d629704401a89e69b452e246805c
Satwik Ram <satwikram29@gmail.com>
2021-05-05 16:38:26 +0530

    fixed

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
@@ -9,6 +10,26 @@ from io import StringIO
 import urllib.request
 import tensorflow as tf
 import numpy as np
+from .serializers import *
+import os
+import shutil
+import matplotlib.pyplot as plt
+from .models import *
+
+def normalize(input_image):
+
+    #input_image = tf.constant(input_image)
+    input_image = tf.image.resize(input_image, (128, 128))
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+
+    return input_image
+
+def create_mask(pred_mask):
+
+  pred_mask = tf.argmax(pred_mask, axis=-1)
+  pred_mask = pred_mask[..., tf.newaxis]
+
+  return pred_mask[0]
 
 class ImageSegmentationAPIView(APIView):
 
@@ -24,8 +45,42 @@ class ImageSegmentationAPIView(APIView):
 
         image = request.data['image']
 
-        print(image.name)
-
         model = tf.keras.models.load_model('segment.hdf5')
 
-        return Response({"Status": "Sucess!"})
+        #image = normalize(image)
+        print(image)
+        folder = 'uploads/images/'
+
+        fs = FileSystemStorage(location=folder)
+        name = fs.save(image.name, image)
+
+        mediapath = folder + "{}"
+        filepath = os.path.join(mediapath).format(name)
+        print("File path:", filepath)
+
+        input_image = plt.imread(filepath)
+
+        input_image = normalize(input_image)
+
+        print(input_image.shape)
+        prediction = model.predict(input_image[tf.newaxis, ...])
+
+        prediction = create_mask(prediction)
+
+        prediction = tf.image.resize(prediction, (480, 480))
+
+        prediction = tf.keras.preprocessing.image.array_to_img(prediction)
+
+        path = 'uploads/predictions/'+name
+        print(path)
+        plt.imsave(path, prediction)
+
+        data = {'image': filepath,
+                'prediction': path}
+
+        serializer = SegmentationSerializer(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+        return Response({"Status": serializer.data})
