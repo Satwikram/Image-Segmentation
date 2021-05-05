b9d09a36baac3b05c404abac4858c1f86c27effe
Satwik Ram <satwikram29@gmail.com>
2021-05-05 16:37:31 +0530

    Admin Page

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Segmentation)
+class SegmentModel(admin.ModelAdmin):
+
+    list_filter = ('id', 'image', 'prediction')
+    list_display = ('id', 'image', 'prediction')
+
